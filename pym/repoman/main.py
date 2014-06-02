435dc973922593ab6ee536339763512c72f6a8f6
Brian Dolbec <dolsen@gentoo.org>
2014-06-01 18:01:31 -0700

    repoman/main.py: Rename manifester return variable 'skip' to 'continue_'

--- pym/repoman/main.py
+++ pym/repoman/main.py
@@ -333,8 +333,8 @@ for xpkg in effective_scanlist:
 
 #####################
 	manifester = Manifests(options, repoman_settings)
-	skip = manifester.run(checkdir, portdb)
-	if skip:
+	continue_ = manifester.run(checkdir, portdb)
+	if continue_:
 		continue
 ######################
 
