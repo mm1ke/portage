24b2088aab8aa754b7b2dd9023d8ba69ea3141f5
Tom Wijsman <tomwij@gentoo.org>
2014-06-06 14:24:18 +0200

    repoman: Apply PEP 8 guidelines again (except for W191)

--- pym/repoman/qa_data.py
+++ pym/repoman/qa_data.py
@@ -6,8 +6,6 @@ from _emerge.Package import Package
 # import our initialized portage instance
 from repoman._portage import portage
 
-
-
 # 14 is the length of DESCRIPTION=""
 max_desc_len = 100
 allowed_filename_chars = "a-zA-Z0-9._-+:"
@@ -426,5 +424,3 @@ def format_qa_output_column(
 			for failure in fails_list:
 				formatter.add_literal_data(category + " " + failure)
 				formatter.add_line_break()
-
-
